a34aa10d75c7e9f84a019ebd1d5fc3071d9e18c4
MarcadiaInc <julien.hubert@outlook.com>
2020-05-28 17:08:24 +0200

    Removed hour from transaction

--- commands/transaction.js
+++ commands/transaction.js
@@ -182,7 +182,7 @@ module.exports = {
             }
         });
 
-        const date = new Intl.DateTimeFormat(language, { day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit' }).format(Date.now());
+        const date = new Intl.DateTimeFormat(language, { day: '2-digit', month: '2-digit', year: 'numeric' }).format(Date.now());
 
         const appendedRow = await sheets.spreadsheets.values.append({
             spreadsheetId,
